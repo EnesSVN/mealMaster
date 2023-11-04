fc22e78235f9b918ff3eb23f4801db4b0c48f8a2
giriftzen <enes.seveng37@gmail.com>
2023-11-04 22:40:15 +0300

    [CREATED] input and reservation

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -2,6 +2,7 @@ import About from "@/components/layout/About";
 import Campaigns from "@/components/layout/Campaigns";
 import Carousel from "@/components/layout/Carousel";
 import MenuWrapper from "@/components/product/MenuWrapper";
+import Reservation from "@/components/layout/Reservation";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Campaigns />
       <MenuWrapper />
       <About />
+      <Reservation />
     </div>
   );
 };
