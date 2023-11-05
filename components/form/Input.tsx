91a6956ba4a31676880dcea603c96c0c8eb72a70
giriftzen <enes.seveng37@gmail.com>
2023-11-05 13:55:44 +0300

    [EDİTED] inputs

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 
-function Input() {
+type InputProps = {
+  type: string;
+  name: string;
+  placeholder: string;
+};
+
+function Input({ type, name, placeholder }: InputProps) {
   return (
     <div className=" w-full">
       <label className=" relative block cursor-text w-full">
         <input
-          type="text"
-          className="h-14 w-full border border-primary outline-none px-4 peer pt-2"
+          className={
+            "h-14 w-full border border-primary outline-none px-4 peer" +
+            (type !== "date-ime-local" && " pt-2")
+          }
           required
+          name={name}
+          type={type}
         />
-        <span className=" absolute top-0 left-0 px-4 text-sm flex items-center h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h-7 peer-valid:text-xs transition-all duration-100">
-          Email
-        </span>
+        {type !== "datetime-local" && (
+          <span className=" absolute top-0 left-0 px-4 text-sm flex items-center h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h-7 peer-valid:text-xs transition-all duration-100">
+            {placeholder}
+          </span>
+        )}
       </label>
     </div>
   );
--- components/layout/Reservation.tsx
+++ components/layout/Reservation.tsx
@@ -3,34 +3,56 @@ import Title from "../ui/Title";
 import Input from "../form/Input";
 
 function Reservation() {
-  return (
-    <div className=" container mx-auto py-12">
-      <div className="flex justify-between flex-wrap items-center gap-x-10">
-        <div className=" lg:flex-1 w-full">
-          <Title text="Book A Table" MyClassName="text-4xl mb-10" />
-          <div className=" flex flex-col gap-y-5">
-            <Input />
-            <Input />
-            <Input />
-            <Input />
-            <Input />
+
+    const inputs = [
+      { id: 1, type: "text", name: "fullName", placeholder: "Your Full Name" },
+      {
+        id: 2,
+        type: "Number",
+        name: "phoneNumber",
+        placeholder: "Your Phone Number",
+      },
+      { id: 3, type: "email", name: "email", placeholder: "Your Email" },
+      {
+        id: 4,
+        type: "Number",
+        name: "persons",
+        placeholder: "How Many Persons?",
+      },
+      { id: 5, type: "datetime-local", name: "date", placeholder: "Date" },
+    ];
+
+    return (
+      <div className=" container mx-auto py-12">
+        <div className="flex justify-between flex-wrap-reverse items-center gap-x-10">
+          <div className=" lg:flex-1 w-full">
+            <Title text="Book A Table" MyClassName="text-4xl mb-10" />
+            <div className=" flex flex-col gap-y-5">
+              {inputs.map((input) => (
+                <Input
+                  key={input.id}
+                  type={input.type}
+                  name={input.name}
+                  placeholder={input.placeholder}
+                />
+              ))}
+            </div>
+            <button className=" btn-primary my-4 ">BOOK NOW</button>
           </div>
-          <button className=" btn-primary my-4 ">BOOK NOW</button>
-        </div>
-        <div className=" lg:flex-1 w-full flex justify-center">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6499.994588797598!2d-73.82597551521248!3d40.76520104811015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c261ccc015963d%3A0x2eca844d8a81ac6!2sWitch%20Topokki%20%7C%20AYCE%20Korean%20Cuisine%20Topokki%20Queens%2C%20NY!5e0!3m2!1str!2str!4v1699125671213!5m2!1str!2str"
-            width="600"
-            height="450"
-            className="border-0"
-            allowFullScreen={true}
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          <div className=" lg:flex-1 w-full flex justify-center">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6499.994588797598!2d-73.82597551521248!3d40.76520104811015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c261ccc015963d%3A0x2eca844d8a81ac6!2sWitch%20Topokki%20%7C%20AYCE%20Korean%20Cuisine%20Topokki%20Queens%2C%20NY!5e0!3m2!1str!2str!4v1699125671213!5m2!1str!2str"
+              width="600"
+              height="450"
+              className="border-0"
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default Reservation;
