65e96b05d8baafecda7f86861ac44f58e8589391
EnesSVN <enessvndev@gmail.com>
2023-11-08 15:52:09 +0300

    [CREATED] footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/layout/Header";
 import Home from "./home";
 import Input from "@/components/form/Input";
+import Footer from "@/components/layout/Footer";
 
 export default function Index() {
   return (
     <main>
       <Header />
       <Home />
+      <Footer />
     </main>
   );
 }
