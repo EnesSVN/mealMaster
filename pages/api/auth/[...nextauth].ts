6efe885b9bd6b6402319c03b1d9954b693ce9920
EnesSVN <enessvndev@gmail.com>
2023-11-30 11:06:32 +0300

    [CREATED] mongoDb adoptor [EDITED] NextAuth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,14 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GithupProvider from "next-auth/providers/github";
-
+import clientPromise from "../../../util/mongo";
 type GithubProvider = {
   clientId: string;
   clientSecret: string;
 };
 
 export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithupProvider({
       clientId: process.env.GITHUB_ID,
