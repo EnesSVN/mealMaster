5c32678a065aa65a68709770edbdff1dccdcf411
EnesSVN <enessvndev@gmail.com>
2023-12-01 09:29:31 +0300

    [CREATED] register

--- models/User.tsx
+++ models/User.tsx
@@ -13,12 +13,9 @@ const UserSchema = new mongoose.Schema(
     },
     phoneNumber: {
       type: String,
-      required: true,
-      unique: true,
     },
     address: {
       type: String,
-      required: true,
     },
     job: {
       type: String,
